96ef212797c576fc4f1c00b874c738ea092be406
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-05-27 18:19:34 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,3 +1,15 @@
 # Use this space to code along with the readme
 
-
+def say_Hwtt
+  phrase = "Hello World"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase 
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
